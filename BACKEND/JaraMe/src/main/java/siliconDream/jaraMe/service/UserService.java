37c200cd97ab0524417e250434e0b0d9c586f098
Hannah1011 <108389766+Hannah1011@users.noreply.github.com>
2024-01-21 21:56:58 +0900

    Update UserService.java

--- BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/service/UserService.java
+++ BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/service/UserService.java
@@ -19,4 +19,16 @@ public interface UserService {
 
     //수정한 부분
     User findUserByUserId(Long userId);
+
+    //로그인 메소드
+    User login(String email, String password);
+
+    User findUserByEmail(String email);
+
+    // 회원 탈퇴
+    void deleteUser(Long userId);
+
+    // 프로필 사진 업로드 및 수정
+    void updateProfileImage(Long userId, String profileImagePath);
+
 }
