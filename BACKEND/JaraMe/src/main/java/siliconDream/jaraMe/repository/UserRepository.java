490b848663bcb7b9a6dd3159252dd2d400261318
Hannah1011 <108389766+Hannah1011@users.noreply.github.com>
2024-01-21 21:54:32 +0900

    Update UserRepository.java
    - 이메일로 사용자 찾기 추가

--- BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/repository/UserRepository.java
+++ BACKEND/JaraMe/src/main/java/siliconDream/jaraMe/repository/UserRepository.java
@@ -12,5 +12,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     //수정한 부분
     User findByUserId(Long userId);
 
+    //이메일로 사용자 찾기
+    User findByEmail(String email);
 
 }
